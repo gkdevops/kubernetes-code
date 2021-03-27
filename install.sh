310a4fbc3732c944ce1af4d2f3c6d834d42b576b
Goutham Kumar <47983326+gkdevops@users.noreply.github.com>
2021-03-27 21:35:22 +0530

    updated with new ubuntu commands

--- install.sh
+++ install.sh
@@ -23,10 +23,8 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Install Kubernetes
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
