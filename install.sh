a3ab9392c6132fabe70703acb1a05603e6d43d84
Goutham Kumar <47983326+gkdevops@users.noreply.github.com>
2021-08-19 06:58:33 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,8 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
+#sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet=1.21.1-00 kubeadm=1.21.1-00 kubectl=1.21.1-00 #--allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl
 
 # CNI Plugin Installation:
